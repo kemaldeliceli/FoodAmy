ba8704593e58f9e304a855985ef9d8255557f02a
kemal <kmldeliceli@gmail.com>
2021-08-28 20:45:35 +0300

    Window background added.

--- app/src/main/java/com/lesson/foodamy/MainActivity.kt
+++ app/src/main/java/com/lesson/foodamy/MainActivity.kt
@@ -1,15 +1,8 @@
 package com.lesson.foodamy
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
-import com.lesson.foodamy.adapter.OnboardingItemsAdapter
-import com.lesson.foodamy.databinding.ActivityMainBinding
-import com.lesson.foodamy.model.OnboardingItem
+
 
 class MainActivity : AppCompatActivity() {
 
