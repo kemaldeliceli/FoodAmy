2ddf4dd5bf6fbcadb8d5bf3b484dbcf14e05b41f
kemal <kmldeliceli@gmail.com>
2021-09-16 14:47:26 +0300

    User response is sent to homepage.

--- app/src/main/java/com/lesson/foodamy/view/LoginFragment.kt
+++ app/src/main/java/com/lesson/foodamy/view/LoginFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat.getColor
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.androidadvance.topsnackbar.TSnackbar
 import com.lesson.foodamy.R
 import com.lesson.foodamy.databinding.FragmentLoginBinding
@@ -16,7 +17,7 @@ import com.lesson.foodamy.services.AuthAPIService
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
-    private lateinit var binding :FragmentLoginBinding
+    private lateinit var binding: FragmentLoginBinding
     private lateinit var snackbar: TSnackbar
 
 
@@ -26,45 +27,63 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         setListeners()
 
         return binding.root
     }
-    private fun setSnackbar(message: String){
-        snackbar = TSnackbar.make(binding.snackbarCoord,message,TSnackbar.LENGTH_LONG)
+
+    private fun setSnackbar(message: String) {
+        snackbar = TSnackbar.make(binding.snackbarCoord, message, TSnackbar.LENGTH_LONG)
 
         val snackbarView = snackbar.view
-        snackbarView.setBackgroundColor(getColor(resources, R.color.primary_red,null))
+        snackbarView.setBackgroundColor(getColor(resources, R.color.primary_red, null))
 
-        val textView :TextView = snackbarView.findViewById(com.androidadvance.topsnackbar.R.id.snackbar_text)
+        val textView: TextView =
+            snackbarView.findViewById(com.androidadvance.topsnackbar.R.id.snackbar_text)
 
-        textView.setTextColor(getColor(resources, R.color.neutral_white,null))
+        textView.setTextColor(getColor(resources, R.color.neutral_white, null))
     }
-    private fun setListeners(){
+
+    private fun setListeners() {
         binding.loginButton.setOnClickListener {
-            val userAuth = AuthData(binding.userEmailEditText.text.toString(),binding.userPasswordEditText.text.toString())
-            //val userAuth = AuthData("admin@fodamy.com","Fodamy@fodamy2017MST")
-            val response = validateEmail(userAuth)
-            if (response==true){
-                AuthAPIService.requestAuth(userAuth)
+            val userAuth = AuthData(
+                binding.userEmailEditText.text.toString(),
+                binding.userPasswordEditText.text.toString()
+            )
+            if (isLoginFieldsValid(userAuth)) {
+                val responseMessage = AuthAPIService.requestAuth(userAuth)
+
+                responseMessage.responseUser?.user?.let {
+                            setSnackbar("Successful Login: " + it.email)
+                            snackbar.show()
+                            val action = LoginFragmentDirections.actionLoginFragmentToMainFragment(it)
+                            findNavController().navigate(action)
+                    }
+
+                responseMessage.errorBody?.let {
+                    setSnackbar(it.error!!)
+                    snackbar.show()
+                }
+
             }
         }
     }
-    private fun validateEmail(userAuth: AuthData): Boolean{
 
-        if (userAuth.email.isEmpty()){
+    private fun isLoginFieldsValid(userAuth: AuthData): Boolean {
+
+        if (userAuth.email.isEmpty()) {
             setSnackbar(getString(R.string.empty_email_blank))
             snackbar.show()
             return false
         }
-        if (userAuth.password.isEmpty()){
+        if (userAuth.password.isEmpty()) {
             setSnackbar(getString(R.string.empty_password_blank))
             snackbar.show()
             return false
         }
-        if (!Patterns.EMAIL_ADDRESS.matcher(userAuth.email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(userAuth.email).matches()) {
             setSnackbar(getString(R.string.wrong_format_email))
             snackbar.show()
             return false
