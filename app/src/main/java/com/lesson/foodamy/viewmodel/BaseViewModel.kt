beee86afc564993a230e0de5dee2f38f484e520f
kemal <kmldeliceli@gmail.com>
2022-01-25 12:58:29 +0300

    Error message change mutable to singleLiveData.

--- app/src/main/java/com/lesson/foodamy/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/lesson/foodamy/viewmodel/BaseViewModel.kt
@@ -1,12 +1,17 @@
 package com.lesson.foodamy.viewmodel
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.lesson.foodamy.SingleLiveEvent
+import com.lesson.foodamy.repository.AuthAPIRepository
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
 
 open class BaseViewModel : ViewModel() {
-    private var _errorMessage: MutableLiveData<Int> = MutableLiveData()
-
+    private var _errorMessage: SingleLiveEvent<Int> = SingleLiveEvent()
+    
     val errorMessage: LiveData<Int>
         get() = _errorMessage
 
