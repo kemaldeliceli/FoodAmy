59c55210e3fc879cd01602788f31794b0b8e4cf5
kemal <kmldeliceli@gmail.com>
2022-01-27 17:39:15 +0300

    Page adapter removed and BottomNavBar used with NavigationHost.

--- app/src/main/java/com/lesson/foodamy/view/MainFragment.kt
+++ app/src/main/java/com/lesson/foodamy/view/MainFragment.kt
@@ -5,11 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.viewpager2.widget.ViewPager2
+import androidx.navigation.ui.setupWithNavController
 import com.lesson.foodamy.R
-import com.lesson.foodamy.adapter.MainPageAdapter
 import com.lesson.foodamy.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -25,44 +25,23 @@ class MainFragment : Fragment() {
         binding.userInfo = args.responseUserInfo
 
         setListeners()
-        setViewPager()
-
-        //ViewPager Page Changing Control
-        binding.mainViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                when(position){
-                    0 -> binding.bottomNavigationView.selectedItemId = R.id.nav_home_icon
-                    1 -> binding.bottomNavigationView.selectedItemId = R.id.nav_fav_icon
-                    2 -> binding.bottomNavigationView.selectedItemId = R.id.nav_user_icon
-                    3 -> binding.bottomNavigationView.selectedItemId = R.id.nav_menu_icon
-                }
-            }
-        })
-        binding.bottomNavigationView.setOnItemSelectedListener { item->
-            when(item.itemId){
-                R.id.nav_home_icon -> binding.mainViewPager.currentItem = 0
-                R.id.nav_fav_icon -> binding.mainViewPager.currentItem = 1
-                R.id.nav_user_icon -> binding.mainViewPager.currentItem = 2
-                R.id.nav_menu_icon -> binding.mainViewPager.currentItem = 3
-            }
-            return@setOnItemSelectedListener true
-        }
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bottomNavigationView = binding.bottomNavigationView
+        val navController = childFragmentManager.findFragmentById(binding.fragmentContainerView .id) as NavHostFragment
+        
+        bottomNavigationView.setupWithNavController(navController.navController)
     }
 
     fun setListeners(){
         binding.logoutIcon.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
         }
-    }
-    fun setViewPager(){
-        binding.mainViewPager.adapter = MainPageAdapter(this)
-    }
-
-    fun setBottomNavigation(){
-        binding.mainViewPager.currentItem = binding.bottomNavigationView.selectedItemId
     }
 
 }
