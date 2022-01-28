7bb443712a9ce50b9e9e6a48de4e62a77209ffc2
kemal <kmldeliceli@gmail.com>
2022-01-28 17:27:34 +0300

    UserInfo injected to fragment and UI's userInfo object updated.

--- app/src/main/java/com/lesson/foodamy/view/mainpageviews/UserFragment.kt
+++ app/src/main/java/com/lesson/foodamy/view/mainpageviews/UserFragment.kt
@@ -5,9 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.lesson.foodamy.Preferences.IPrefDefaultManager
 import com.lesson.foodamy.R
+import com.lesson.foodamy.databinding.FragmentUserBinding
+import com.lesson.foodamy.model.UserInfo
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.combine
+import javax.inject.Inject
+
+@AndroidEntryPoint
 
 class UserFragment : Fragment() {
+
+    @Inject
+    lateinit var loginSharedPref: IPrefDefaultManager
+    lateinit var binding: FragmentUserBinding
+    lateinit var userInfo: UserInfo
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,7 +31,11 @@ class UserFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false)
+        binding = FragmentUserBinding.inflate(inflater,container,false)
+
+        userInfo = loginSharedPref.getUserInfo()
+        binding.userInfo = this.userInfo
+        return binding.root
     }
 
 }
