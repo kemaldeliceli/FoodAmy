11094efac95b915209ccc8906a2af82b91a6afe0
kemal <kmldeliceli@gmail.com>
2022-02-02 18:01:20 +0300

    Data classes for recipes taken from API.

---
+++ app/src/main/java/com/lesson/foodamy/model/recipe_dataclass/Images.kt
@@ -0,0 +1,12 @@
+package com.lesson.foodamy.model.recipe_dataclass
+
+
+data class Images (
+
+  val width  : Int?    = null,
+  val height : Int?    = null,
+  val key    : String? = null,
+  val order  : Int?    = null,
+  val url    : String? = null
+
+)
