2146698b024965c97e5c75dfee659f77f3d06ed3
kemal <kmldeliceli@gmail.com>
2022-02-02 18:09:15 +0300

    Basic changes like rename class, move file and change var to val.

--- app/src/main/java/com/lesson/foodamy/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/lesson/foodamy/ui/login/LoginViewModel.kt
@@ -1,4 +1,4 @@
-package com.lesson.foodamy.viewmodel
+package com.lesson.foodamy.ui.login
 
 import android.util.Patterns
 import androidx.lifecycle.LiveData
@@ -7,13 +7,14 @@ import androidx.lifecycle.viewModelScope
 import com.lesson.foodamy.R
 import com.lesson.foodamy.model.*
 import com.lesson.foodamy.model.dataclass.AuthData
-import com.lesson.foodamy.repository.AuthAPIRepository
+import com.lesson.foodamy.repository.AuthApiRepository
+import com.lesson.foodamy.core.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(private val authAPIRepository: AuthAPIRepository) : BaseViewModel() {
+class LoginViewModel @Inject constructor(private val authAPIRepository: AuthApiRepository) : BaseViewModel() {
     private var _responseMessage: MutableLiveData<BaseResponse<ResponseUser>> = MutableLiveData()
     val email: MutableLiveData<String> = MutableLiveData("")
     val password: MutableLiveData<String> = MutableLiveData("")
