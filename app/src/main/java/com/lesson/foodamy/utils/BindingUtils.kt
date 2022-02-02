02b3ba9a611d695b91bd08b8aec17dd76a5eff46
kemal <kmldeliceli@gmail.com>
2022-02-02 18:08:41 +0300

    Binding adapter for image load to imageView via Glide.

--- app/src/main/java/com/lesson/foodamy/utils/BindingUtils.kt
+++ app/src/main/java/com/lesson/foodamy/utils/BindingUtils.kt
@@ -1,7 +1,9 @@
-package com.lesson.foodamy
+package com.lesson.foodamy.utils
 
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 object BindingUtils {
 
@@ -10,5 +12,10 @@ object BindingUtils {
     fun updateVisibility(view: View, visible: Boolean) {
         view.visibility = if (visible) View.VISIBLE else View.GONE
     }
+    @JvmStatic
+    @BindingAdapter("app:imageUrl")
+    fun loadImage(view: ImageView, url: String?) {
+        Glide.with(view.context).load(url)
+            .into(view)    }
 
 }
