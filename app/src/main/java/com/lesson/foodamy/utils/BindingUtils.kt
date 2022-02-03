75827416a3a199cdcf425d365751cc2e4fdd3624
kemal <kmldeliceli@gmail.com>
2022-02-03 16:58:13 +0300

    Load Image BindingAdapter was added to load image via Glide.

--- app/src/main/java/com/lesson/foodamy/utils/BindingUtils.kt
+++ app/src/main/java/com/lesson/foodamy/utils/BindingUtils.kt
@@ -1,9 +1,13 @@
 package com.lesson.foodamy.utils
 
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
+import androidx.constraintlayout.widget.Placeholder
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.lesson.foodamy.R
 
 object BindingUtils {
 
@@ -13,9 +17,19 @@ object BindingUtils {
         view.visibility = if (visible) View.VISIBLE else View.GONE
     }
     @JvmStatic
-    @BindingAdapter("app:imageUrl")
-    fun loadImage(view: ImageView, url: String?) {
-        Glide.with(view.context).load(url)
-            .into(view)    }
+    @BindingAdapter("app:imageUrl","app:placeHolder")
+    fun loadImage(view: ImageView, url: String?,placeholder: Drawable) {
+        println(url)
+        when {
+            url.isNullOrEmpty() -> {
+                view.setImageDrawable(placeholder)
+            }
+            else -> {
+                Glide.with(view.context).load(url)
+                    .priority(Priority.HIGH)
+                    .into(view)
+            }
+        }
+    }
 
 }
