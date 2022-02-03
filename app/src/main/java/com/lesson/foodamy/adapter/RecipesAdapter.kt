1e4d4da3d53dfe271acc15e99ae69a182b68bb99
kemal <kmldeliceli@gmail.com>
2022-02-03 17:04:35 +0300

    Unnecessary impors was removed.

--- app/src/main/java/com/lesson/foodamy/adapter/RecipesAdapter.kt
+++ app/src/main/java/com/lesson/foodamy/adapter/RecipesAdapter.kt
@@ -1,16 +1,9 @@
 package com.lesson.foodamy.adapter
 
-import android.text.Layout
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.lesson.foodamy.R
-import com.lesson.foodamy.databinding.FragmentIntroBinding
 import com.lesson.foodamy.databinding.RecipeCardViewBinding
 import com.lesson.foodamy.model.recipe_dataclass.Data
 
