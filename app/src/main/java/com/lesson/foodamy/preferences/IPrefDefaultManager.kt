7c47ca98d9472abd5a1e92122386a6e73e5e2773
kemal <kmldeliceli@gmail.com>
2022-02-09 14:47:46 +0300

    Variable name changed.

--- app/src/main/java/com/lesson/foodamy/preferences/IPrefDefaultManager.kt
+++ app/src/main/java/com/lesson/foodamy/preferences/IPrefDefaultManager.kt
@@ -5,6 +5,6 @@ import com.lesson.foodamy.model.UserInfo
 interface IPrefDefaultManager {
     fun isAppFirstOpen():Boolean
     fun saveAppOpened()
-    fun setUserInfo(`object`: UserInfo)
-    fun getUserInfo():UserInfo
+    fun setUserInfo(userInfo: UserInfo)
+    fun getUserInfo():UserInfo?
 }
