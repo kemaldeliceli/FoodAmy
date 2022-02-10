807c6a7bf24d1ac5a529618149e58b27d9633e3d
kemal <kmldeliceli@gmail.com>
2022-02-10 12:14:02 +0300

    Save and get user is logged or not.

--- app/src/main/java/com/lesson/foodamy/preferences/SharedPrefManager.kt
+++ app/src/main/java/com/lesson/foodamy/preferences/SharedPrefManager.kt
@@ -3,12 +3,13 @@ package com.lesson.foodamy.preferences
 import android.content.SharedPreferences
 import androidx.core.content.edit
 import com.google.gson.GsonBuilder
-import com.lesson.foodamy.model.UserInfo
+import com.lesson.foodamy.model.UserInformation
 
 class SharedPrefManager(private val sharedPreferences: SharedPreferences) : IPrefDefaultManager {
 
     companion object{
         private const val firstTimeAppOpen = "firstTimeAppOpen"
+        private const val isLoggedIn="isLoggedIn"
         private const val userInfoObject = "userInfoObject"
     }
 
@@ -18,22 +19,28 @@ class SharedPrefManager(private val sharedPreferences: SharedPreferences) : IPre
             }
     }
 
+    override fun isLoggedIn() = sharedPreferences.getBoolean(isLoggedIn,false)
 
+    override fun saveLogin(isLogged:Boolean){
+        sharedPreferences.edit(commit = true){
+            putBoolean(isLoggedIn,isLogged)
+        }
+    }
 
     override fun isAppFirstOpen() = sharedPreferences.getBoolean(firstTimeAppOpen,false)
 
-    override fun setUserInfo(user: UserInfo) {
+    override fun setUserInfo(user: UserInformation) {
         val jsonString = GsonBuilder().create().toJson(user)
         //Save that String in SharedPreferences
         sharedPreferences.edit().putString(userInfoObject, jsonString).apply()
     }
 
-    override fun getUserInfo(): UserInfo? {
+    override fun getUserInfo(): UserInformation? {
         val value = sharedPreferences.getString(userInfoObject, null)
         //JSON String was found which means object can be read.
         //We convert this JSON String to model object. Parameter "c" (of
         //type Class < T >" is used to cast.
-        return GsonBuilder().create().fromJson(value, UserInfo::class.java)
+        return GsonBuilder().create().fromJson(value, UserInformation::class.java)
     }
 
 
