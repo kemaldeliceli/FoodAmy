1e2c68f425525f277377f668023a7b13da320eaf
kemal <kmldeliceli@gmail.com>
2022-02-10 12:18:12 +0300

    Sub data class added and this is returned by toResult() function.

--- app/src/main/java/com/lesson/foodamy/model/UserInfo.kt
+++ app/src/main/java/com/lesson/foodamy/model/UserInfo.kt
@@ -1,45 +1,75 @@
 package com.lesson.foodamy.model
 
-import android.annotation.SuppressLint
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import com.lesson.foodamy.model.recipe_dataclass.User
+import kotlinx.parcelize.Parcelize
 
-@SuppressLint("ParcelCreator")
+@Parcelize
 data class UserInfo (
-    @SerializedName("id") var id : Int?,
-    @SerializedName("email") var email : String?,
-    @SerializedName("username") var username : String?,
-    @SerializedName("name") var name : String?,
-    @SerializedName("surname") var surname : String?,
-    @SerializedName("definition") var definition : String?,
-    @SerializedName("phone") var phone : String?,
-    @SerializedName("tckn") var tckn : String?,
-    @SerializedName("gender") var gender : String?,
-    @SerializedName("birthday") var birthday : String?,
-    @SerializedName("is_banned") var isBanned : Int?,
-    @SerializedName("is_trusted") var isTrusted : Int?,
-    @SerializedName("facebook_url") var facebookUrl : String?,
-    @SerializedName("twitter_url") var twitterUrl : String?,
-    @SerializedName("instagram_url") var instagramUrl : String?,
-    @SerializedName("youtube_url") var youtubeUrl : String?,
-    @SerializedName("language") var language : String?,
-    @SerializedName("is_top_user_choice") var isTopUserChoice : Boolean?,
-    @SerializedName("followed_count") var followedCount : Int?,
-    @SerializedName("following_count") var followingCount : Int?,
-    @SerializedName("recipe_count") var recipeCount : Int?,
-    @SerializedName("is_following") var isFollowing : Boolean?,
-    @SerializedName("favorites_count") var favoritesCount : Int?,
-    @SerializedName("likes_count") var likesCount : Int?,
-    @SerializedName("cover") var cover : String?,
-    @SerializedName("image") var image : String?,
-    @SerializedName("top_user") var topUser : String?,
-    @SerializedName("cover_image") var coverImage : String?
-) :Parcelable {
-    override fun describeContents(): Int {
-        return Parcelable.CONTENTS_FILE_DESCRIPTOR
-    }
+    val id : Int?,
+    val email : String?,
+    val username : String?,
+    val name : String?,
+    val surname : String?,
+    val definition : String?,
+    val phone : String?,
+    val tckn : String?,
+    val gender : String?,
+    val birthday : String?,
+    @SerializedName("is_banned") val isBanned : Int?,
+    @SerializedName("is_trusted") val isTrusted : Int?,
+    @SerializedName("facebook_url") val facebookUrl : String?,
+    @SerializedName("twitter_url") val twitterUrl : String?,
+    @SerializedName("instagram_url") val instagramUrl : String?,
+    @SerializedName("youtube_url") val youtubeUrl : String?,
+    val language : String?,
+    @SerializedName("is_top_user_choice") val isTopUserChoice : Boolean?,
+    @SerializedName("followed_count") val followedCount : Int?,
+    @SerializedName("following_count") val followingCount : Int?,
+    @SerializedName("recipe_count") val recipeCount : Int?,
+    @SerializedName("is_following") val isFollowing : Boolean?,
+    @SerializedName("favorites_count") val favoritesCount : Int?,
+    @SerializedName("likes_count") val likesCount : Int?,
+    val cover : String?,
+    val image : String?,
+    @SerializedName("top_user") val topUser : String?,
+    @SerializedName("cover_image") val coverImage : String?
+) :Parcelable
 
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
-    }
+@Parcelize
+data class UserInformation(
+    val id : Int?,
+    val email : String?,
+    val username : String?,
+    val name : String?,
+    val surname : String?,
+    val followedCount : Int?,
+    val followingCount : Int?,
+    val recipeCount : Int?,
+    val isFollowing : Boolean?,
+    val favoritesCount : Int?,
+    val likesCount : Int?,
+    val cover : String?,
+    val image : String?,
+    val coverImage : String?
+    ):Parcelable
+
+fun UserInfo.toResult(): UserInformation{
+    return UserInformation(
+            this.id,
+            this.email,
+            this.username,
+            this.name,
+            this.surname,
+            this.followedCount,
+            this.followingCount,
+            this.recipeCount,
+            this.isFollowing,
+            this.favoritesCount,
+            this.likesCount,
+            this.cover,
+            this.image,
+            this.coverImage
+    )
 }
