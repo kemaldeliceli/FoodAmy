9d66540988b4d4a8cab6f8304e9531224fc05e8a
kemal <kmldeliceli@gmail.com>
2022-02-11 12:43:29 +0300

    Logout response control and navigate to login page process moved to viewModel.

--- app/src/main/java/com/lesson/foodamy/ui/main/MainFragment.kt
+++ app/src/main/java/com/lesson/foodamy/ui/main/MainFragment.kt
@@ -2,60 +2,47 @@ package com.lesson.foodamy.ui.main
 
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.fragment.findNavController
 import com.lesson.foodamy.R
 import com.lesson.foodamy.adapter.MainPageAdapter
 import com.lesson.foodamy.core.BaseFragment
 import com.lesson.foodamy.databinding.FragmentMainBinding
-import com.lesson.foodamy.model.BaseResponse
-import com.lesson.foodamy.preferences.IPrefDefaultManager
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainFragment : BaseFragment<MainViewModel, FragmentMainBinding>(R.layout.fragment_main) {
-    @Inject
-    lateinit var loginSharedPreferences: IPrefDefaultManager
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setCoordinateSnackbar(binding.snackbarCoord)
         setAdapter()
         setBottomNavigationBar()
-        setListeners()
+        setCoordinateSnackbar(binding.snackbarCoord)
     }
 
     private fun setBottomNavigationBar() {
         binding.viewPager.isUserInputEnabled = false
         binding.viewPager.isMotionEventSplittingEnabled = false
 
-
         binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.homeFragment -> {
-                    //binding.viewPager.currentItem = 0
                     binding.viewPager.setCurrentItem(0, false)
                     true
                 }
                 R.id.favoruiteFragment -> {
-                    //                   binding.viewPager.currentItem = 1
                     binding.viewPager.setCurrentItem(1, false)
                     true
                 }
                 R.id.userFragment -> {
-                    //                 binding.viewPager.currentItem = 2
                     binding.viewPager.setCurrentItem(2, false)
-
                     true
                 }
                 R.id.menuFragment -> {
-//                    binding.viewPager.currentItem = 3
                     binding.viewPager.setCurrentItem(3, false)
 
                     true
                 }
 
                 else -> {
-//                    binding.viewPager.currentItem = 0
                     binding.viewPager.setCurrentItem(0, false)
                     true
                 }
@@ -66,18 +53,6 @@ class MainFragment : BaseFragment<MainViewModel, FragmentMainBinding>(R.layout.f
     private fun setAdapter() {
         binding.viewPager.offscreenPageLimit = 1
         binding.viewPager.adapter = MainPageAdapter(fragment = this)
-    }
-
-    fun setListeners() {
-        viewModel.responseLogout.observe(viewLifecycleOwner, { logoutResponse ->
-            when (logoutResponse) {
-                is BaseResponse.Error -> {setSnackbar(logoutResponse.error.error.toString())}
-                is BaseResponse.Success -> {
-                    setSnackbar("Successfull Logout")
-                    loginSharedPreferences.saveLogin(isLogged = false)
-                    findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
-                }
-            }})
     }
 
     override fun getViewModelss(): Class<MainViewModel> {
--- app/src/main/java/com/lesson/foodamy/ui/main/MainViewModel.kt
+++ app/src/main/java/com/lesson/foodamy/ui/main/MainViewModel.kt
@@ -1,11 +1,10 @@
 package com.lesson.foodamy.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+
 import androidx.lifecycle.viewModelScope
+import com.lesson.foodamy.R
 import com.lesson.foodamy.core.BaseViewModel
 import com.lesson.foodamy.model.BaseResponse
-import com.lesson.foodamy.model.ResponseLogout
 import com.lesson.foodamy.preferences.IPrefDefaultManager
 import com.lesson.foodamy.repository.AuthApiRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,12 +15,19 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(private val loginSharedPreferences: IPrefDefaultManager,
                                         private val authApiRepository: AuthApiRepository
 ) : BaseViewModel() {
-    private val _responseLogout : MutableLiveData<BaseResponse<ResponseLogout>> = MutableLiveData()
-    val responseLogout: LiveData<BaseResponse<ResponseLogout>>
-        get() = _responseLogout
 
     fun logOut() = viewModelScope.launch{
-        _responseLogout.value = authApiRepository.requestLogout()
+        when(val response = authApiRepository.requestLogout()){
+            is BaseResponse.Success->{
+                showMessage(R.string.successfull_logout)
+                loginSharedPreferences.saveLogin(isLogged = false)
+                navigate(MainFragmentDirections.actionMainFragmentToLoginFragment())
+            }
+            is BaseResponse.Error->{
+                showMessage(response.error.error.toString())
+            }
+        }
+
     }
 
 }
