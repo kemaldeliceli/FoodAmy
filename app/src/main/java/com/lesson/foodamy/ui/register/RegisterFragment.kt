7f465c53c18599e16682a7daf198130ccdc50030
kemal <kmldeliceli@gmail.com>
2022-02-11 12:41:24 +0300

    Navigate and showMwssage process moved to viewModel.

--- app/src/main/java/com/lesson/foodamy/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/lesson/foodamy/ui/register/RegisterFragment.kt
@@ -2,11 +2,8 @@ package com.lesson.foodamy.ui.register
 
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import com.lesson.foodamy.R
 import com.lesson.foodamy.databinding.FragmentRegisterBinding
-import com.lesson.foodamy.model.BaseResponse
 import com.lesson.foodamy.core.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,30 +12,7 @@ class RegisterFragment : BaseFragment<RegisterViewModel, FragmentRegisterBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        getResponseAndNavigate()
         setCoordinateSnackbar(binding.snackbarCoord)
-        setListeners()
-    }
-
-    private fun setListeners() {
-        binding.loginText.setOnClickListener {
-            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
-        }
-    }
-
-
-    private fun getResponseAndNavigate() {
-        viewModel.responseMessage.observe(viewLifecycleOwner, Observer<BaseResponse<Any>>{responseMessage ->
-            when(responseMessage){
-                is BaseResponse.Success -> {
-                    setSnackbar(getString(R.string.success_register_process))
-                    findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
-                }
-                is BaseResponse.Error -> {
-                    setSnackbar(responseMessage.error.error.toString())
-                }
-            }
-        })
     }
 
     override fun getViewModelss(): Class<RegisterViewModel> {
--- app/src/main/java/com/lesson/foodamy/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/lesson/foodamy/ui/register/RegisterViewModel.kt
@@ -1,12 +1,10 @@
 package com.lesson.foodamy.ui.register
 
 import android.util.Patterns
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lesson.foodamy.R
 import com.lesson.foodamy.model.BaseResponse
-import com.lesson.foodamy.model.ResponseUser
 import com.lesson.foodamy.model.dataclass.RegisterData
 import com.lesson.foodamy.repository.AuthApiRepository
 import com.lesson.foodamy.core.BaseViewModel
@@ -16,41 +14,48 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RegisterViewModel @Inject constructor(private val authAPIRepository: AuthApiRepository) : BaseViewModel(){
-    private var _responseMessage: MutableLiveData<BaseResponse<ResponseUser>> = MutableLiveData()
     val username : MutableLiveData<String> = MutableLiveData("")
     val email : MutableLiveData<String>  = MutableLiveData("")
     val password: MutableLiveData<String>  = MutableLiveData("")
 
-    val responseMessage: LiveData<BaseResponse<ResponseUser>>
-        get() = _responseMessage
-
     fun register() = viewModelScope.launch {
         val registerData = RegisterData(email.value!!,password.value!!,username.value!!)
         if (isRegisterFieldsValid(registerData)) {
-            _responseMessage.value = authAPIRepository.requestRegister(registerData)
+            val response = authAPIRepository.requestRegister(registerData)
+            when(response){
+                is BaseResponse.Success -> {
+                    showMessage(R.string.success_register_process)
+                    navigateToLogin()
+                }
+                is BaseResponse.Error -> {showMessage(response.error.error.toString())}
+            }
+
         }
     }
 
     fun isRegisterFieldsValid(registerData: RegisterData): Boolean {
         return when{
             registerData.email.isEmpty()-> {
-                showErrorMessage(R.string.empty_email_blank)
+                showMessage(R.string.empty_email_blank)
                 false
             }
             registerData.password.isEmpty()-> {
-                showErrorMessage(R.string.empty_password_blank)
+                showMessage(R.string.empty_password_blank)
                 false
             }
             registerData.username.isEmpty()-> {
-                showErrorMessage(R.string.empty_username_blank)
+                showMessage(R.string.empty_username_blank)
                 false
             }
             !Patterns.EMAIL_ADDRESS.matcher(registerData.email).matches()-> {
-                showErrorMessage(R.string.wrong_format_email)
+                showMessage(R.string.wrong_format_email)
                 false
             }
             else -> true
         }
 
+    }
+    fun navigateToLogin(){
+        navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment())
     }
 }
