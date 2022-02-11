b7161be1f697bbb70c7ebd00c4a9f8b01339209f
kemal <kmldeliceli@gmail.com>
2022-02-11 12:31:11 +0300

    Event LiveEvent added which can be navigate or showMessage. It is observed from BaseFragment.

--- app/src/main/java/com/lesson/foodamy/core/BaseViewModel.kt
+++ app/src/main/java/com/lesson/foodamy/core/BaseViewModel.kt
@@ -1,16 +1,24 @@
 package com.lesson.foodamy.core
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavDirections
 import com.lesson.foodamy.SingleLiveEvent
 
 open class BaseViewModel : ViewModel() {
-    private var _errorMessage: SingleLiveEvent<Int> = SingleLiveEvent()
-    
-    val errorMessage: LiveData<Int>
-        get() = _errorMessage
 
-    fun showErrorMessage(error:Int){
-        _errorMessage.value = error
+    val event = SingleLiveEvent<BaseViewEvent>()
+
+    fun sendEvent(ev: BaseViewEvent){
+        event.postValue(ev)
     }
+
+    fun navigate(directions: NavDirections){
+        sendEvent(BaseViewEvent.Navigate(directions))
+    }
+
+    fun showMessage(msg: Any){
+        sendEvent(BaseViewEvent.ShowMessage(msg))
+    }
+
+
 }
