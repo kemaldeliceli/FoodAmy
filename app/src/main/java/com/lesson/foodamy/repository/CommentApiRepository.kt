0d4de46214ad4a7e38191e5b5ba31c4d276de337
kemal <kmldeliceli@gmail.com>
2022-02-18 12:04:33 +0300

    Comment service and repository created.

--- app/src/main/java/com/lesson/foodamy/repository/CommentApiRepository.kt
+++ app/src/main/java/com/lesson/foodamy/repository/CommentApiRepository.kt
@@ -2,6 +2,7 @@ package com.lesson.foodamy.repository
 
 import com.google.gson.Gson
 import com.lesson.foodamy.model.BaseResponse
+import com.lesson.foodamy.model.comment_dataclass.Comment
 import com.lesson.foodamy.model.comment_dataclass.ResponseComments
 import com.lesson.foodamy.model.dataclass.ErrorBody
 import com.lesson.foodamy.services.CommentService
@@ -32,6 +33,30 @@ class CommentApiRepository (private val commentService: CommentService) {
 
         return  responseComments
     }
+    suspend fun requestAddComments(recipeID: Int , token:String, text: String):  BaseResponse<Comment>?
+    {
+        var responseAddComments : BaseResponse<Comment>? = null
+
+        try {
+            val response =  commentService.addComments(recipeID, token,text)
+
+            if (response.isSuccessful){
+                responseAddComments = BaseResponse.Success(response.body()!!)
+            }else{
+                val errorResponse = response.errorBody()?.string()
+                val errorBody = Gson().fromJson<ErrorBody>(errorResponse, ErrorBody::class.java)
+                responseAddComments = BaseResponse.Error(errorBody)
+            }
+
+        }catch (e: Exception){
+            responseAddComments = BaseResponse.Error(ErrorBody("408","Timeout Error"))
+            println(e.message)
+            println(e.toString())
+        }
+
+        return  responseAddComments
+    }
+
 
 
 }
