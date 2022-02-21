934360631e5dc36d3586791b8b8ffadfe8ab7389
kemal <kmldeliceli@gmail.com>
2022-02-21 04:20:19 +0300

    CustomView will be added for topBar

---
+++ app/src/main/java/com/lesson/foodamy/utils/TopBarView.kt
@@ -0,0 +1,12 @@
+package com.lesson.foodamy.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class TopBarView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : ConstraintLayout(context, attrs) {
+
+
+}
