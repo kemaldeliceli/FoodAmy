28524225ad50080339be390c6433062391c09662
kemal <kmldeliceli@gmail.com>
2022-02-21 04:01:13 +0300

    RecipeCategory model added.

---
+++ app/src/main/java/com/lesson/foodamy/model/recipe_category/CategoryInfo.kt
@@ -0,0 +1,16 @@
+package com.lesson.foodamy.model.recipe_category
+
+import com.google.gson.annotations.SerializedName
+
+
+  data class CategoryInfo (
+
+  @SerializedName("id"               ) var id             : Int?               = null,
+  @SerializedName("name"             ) var name           : String?            = null,
+  @SerializedName("language"         ) var language       : String?            = null,
+  @SerializedName("main_category_id" ) var mainCategoryId : String?            = null,
+  @SerializedName("recipe_count"     ) var recipeCount    : Int?               = null,
+  @SerializedName("recipes"          ) var recipes        : ArrayList<Recipes> = arrayListOf(),
+  @SerializedName("image"            ) var image          : Image?             = Image()
+
+)
---
+++ app/src/main/java/com/lesson/foodamy/model/recipe_category/Pagination.kt
@@ -0,0 +1,15 @@
+package com.lesson.foodamy.model.recipe_category
+
+import com.google.gson.annotations.SerializedName
+
+
+data class Pagination (
+
+  @SerializedName("total"        ) var total       : Int? = null,
+  @SerializedName("per_page"     ) var perPage     : Int? = null,
+  @SerializedName("current_page" ) var currentPage : Int? = null,
+  @SerializedName("last_page"    ) var lastPage    : Int? = null,
+  @SerializedName("first_item"   ) var firstItem   : Int? = null,
+  @SerializedName("last_item"    ) var lastItem    : Int? = null
+
+)
