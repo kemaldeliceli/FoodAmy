c445011380387e56a0da1f1c63cbfc2df583dc5b
kemal <kmldeliceli@gmail.com>
2022-02-21 04:19:00 +0300

    Basic Changes.

--- app/src/main/java/com/lesson/foodamy/ui/main/MainPageAdapter.kt
+++ app/src/main/java/com/lesson/foodamy/ui/main/MainPageAdapter.kt
@@ -2,9 +2,9 @@ package com.lesson.foodamy.ui.main
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.lesson.foodamy.ui.favourite.FavouriteFragment
 import com.lesson.foodamy.ui.home.HomeFragment
 import com.lesson.foodamy.ui.menu.MenuFragment
+import com.lesson.foodamy.ui.recipe_categories.RecipeCategoriesFragment
 import com.lesson.foodamy.ui.user.UserFragment
 
 class MainPageAdapter(val fragment: Fragment): FragmentStateAdapter(fragment)
@@ -16,7 +16,7 @@ class MainPageAdapter(val fragment: Fragment): FragmentStateAdapter(fragment)
     override fun createFragment(position: Int): Fragment {
         return when(position){
             0 -> HomeFragment()
-            1 -> FavouriteFragment()
+            1 -> RecipeCategoriesFragment()
             2 -> UserFragment()
             3 -> MenuFragment()
             else-> HomeFragment()
