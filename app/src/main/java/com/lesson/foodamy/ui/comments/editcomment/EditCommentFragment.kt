f17bb784881452ce5b4fa51395d30ea77b4d714a
kemal <kmldeliceli@gmail.com>
2022-02-28 02:16:47 +0300

    Edit Comment page added.

---
+++ app/src/main/java/com/lesson/foodamy/ui/comments/editcomment/EditCommentFragment.kt
@@ -0,0 +1,30 @@
+package com.lesson.foodamy.ui.comments.editcomment
+
+import android.os.Bundle
+import android.view.View
+import com.lesson.foodamy.R
+import com.lesson.foodamy.core.BaseFragment
+import com.lesson.foodamy.databinding.FragmentEditCommentBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class EditCommentFragment : BaseFragment<EditCommentViewModel,FragmentEditCommentBinding>
+    (R.layout.fragment_edit_comment){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            val args = EditCommentFragmentArgs.fromBundle(it)
+            viewModel.comment = args.comment
+            viewModel.recipeID = args.recipeID
+        }
+        setCoordinateSnackbar(binding.snackbarCoord)
+    }
+
+
+    override fun getViewModelss(): Class<EditCommentViewModel> {
+        return EditCommentViewModel::class.java
+    }
+
+}
---
+++ app/src/main/java/com/lesson/foodamy/ui/comments/editcomment/EditCommentViewModel.kt
@@ -0,0 +1,46 @@
+package com.lesson.foodamy.ui.comments.editcomment
+
+
+import android.content.SharedPreferences
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.lesson.foodamy.R
+import com.lesson.foodamy.core.BaseViewModel
+import com.lesson.foodamy.model.BaseResponse
+import com.lesson.foodamy.model.comment_dataclass.Comment
+import com.lesson.foodamy.preferences.IPrefDefaultManager
+import com.lesson.foodamy.repository.RecipesAPIRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class EditCommentViewModel @Inject constructor(private val recipesAPIRepository: RecipesAPIRepository,
+private val sharedPreferences: IPrefDefaultManager
+                                               ) : BaseViewModel(){
+    var recipeID = 0
+    var comment : Comment?=null
+
+    var commentText = MutableLiveData<String>()
+
+    private var token: String = ""
+
+    init {
+        token = sharedPreferences.getToken()
+    }
+
+    fun editComment() = viewModelScope.launch {
+        when(val response = recipesAPIRepository.
+            requestEditComment(recipeID,comment?.id!!,commentText.value.toString(),token)){
+            is BaseResponse.Error -> {
+                showMessage(response.error.error.toString())
+            }
+            is BaseResponse.Success -> {
+                showMessage(response.data.message)
+            }
+            null -> {
+                showMessage(R.string.null_error)
+            }
+        }
+    }
+}
