da669685cc3e207c0f44e447bd77e2bd27f10ea0
kemal <kmldeliceli@gmail.com>
2022-02-28 02:24:10 +0300

    Page parameter added to last added recipes. Delete and edit comment added.

--- app/src/main/java/com/lesson/foodamy/services/RecipeService.kt
+++ app/src/main/java/com/lesson/foodamy/services/RecipeService.kt
@@ -1,21 +1,29 @@
 package com.lesson.foodamy.services
 
+import com.lesson.foodamy.model.ResponseComment
 import com.lesson.foodamy.model.ResponseLike
 import com.lesson.foodamy.model.comment_dataclass.Comment
 import com.lesson.foodamy.model.comment_dataclass.ResponseComments
 import com.lesson.foodamy.model.recipe_category.ResponseRecipeCategory
-import com.lesson.foodamy.model.recipe_dataclass.RecipeInfo
 import com.lesson.foodamy.model.recipe_dataclass.ResponseRecipes
+import com.lesson.foodamy.model.recipe_detail_info.RecipeDetailInfo
+import com.lesson.foodamy.model.recipe_detail_info.ResponseFollow
 import retrofit2.Response
 import retrofit2.http.*
 
 interface RecipeService {
 
     @GET("api/editor-choices")
-    suspend fun getEditorsChoice(): Response<ResponseRecipes>
+    suspend fun getEditorsChoice(@Query("page") page: Int): Response<ResponseRecipes>
 
     @GET("api/recipe")
-    suspend fun getLastAdded(): Response<ResponseRecipes>
+    suspend fun getLastAdded(@Query("page") page: Int): Response<ResponseRecipes>
+
+    @GET("api/recipe/{recipe_id}")
+    suspend fun getRecipeByID(
+        @Path("recipe_id") recipeId:Int,
+        @Header("X-Fodamy-Token") token: String
+        ) : Response<RecipeDetailInfo>
 
     @GET("/api/recipe/{recipe_id}/comment")
     suspend fun getComments(@Path("recipe_id") recipeId:Int) : Response<ResponseComments>
@@ -25,15 +33,50 @@ interface RecipeService {
     suspend fun addComments(
         @Path("recipe_id") recipeId: Int,
         @Header("X-Fodamy-Token") token: String,
-        @Part("text") text: String
+        @Part("text") text: String,
     ): Response<Comment>
 
-    @POST("/api/recipe/{recipe_id}/like")
-    suspend fun likeComments(
+
+    @DELETE("/api/recipe/{recipe_id}/comment/{comment_id}")
+    suspend fun deleteComment(
         @Path("recipe_id") recipeId: Int,
+        @Path("comment_id") commentId:Int,
         @Header("X-Fodamy-Token") token: String,
-    )
-            : Response<ResponseLike>
+    ) : Response<ResponseComment>
+
+    @FormUrlEncoded
+    @PUT ("/api/recipe/{recipe_id}/comment/{comment_id}")
+    suspend fun editComment(
+        @Path("recipe_id") recipeId: Int,
+        @Path("comment_id") commentId: Int,
+        @Field("text") text: String,
+        @Header("X-Fodamy-Token") token: String,
+
+        ) : Response<ResponseComment>
+
+    @POST("/api/recipe/{recipe_id}/like")
+    suspend fun likeRecipe(
+        @Path("recipe_id") recipeId: Int,
+        @Header("X-Fodamy-Token") token: String
+    ) : Response<ResponseLike>
+
+    @DELETE("/api/recipe/{recipe_id}/like")
+    suspend fun deleteLikeRecipe(
+        @Path("recipe_id" ) recipeId: Int,
+        @Header("X-Fodamy-Token") token: String
+    ) : Response<ResponseLike>
+
+    @POST("/api/user/{followedId}/following")
+    suspend fun followUser(
+        @Path("followedId") followedId: Int,
+        @Header("X-Fodamy-Token") token: String
+    ) : Response<ResponseFollow>
+
+    @DELETE("/api/user/{followedId}/following")
+    suspend fun deleteFollowUser(
+        @Path("followedId") followedId: Int,
+        @Header("X-Fodamy-Token") token: String
+    ) : Response<ResponseFollow>
 
     @GET("/api/category-recipes")
     suspend fun getRecipeCategories(): Response<ResponseRecipeCategory>
