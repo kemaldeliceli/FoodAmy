d86dd1ad22820add0806acf09993af8086753b9c
mgolcu00 <mgolcu00@gmail.com>
2022-03-08 16:14:46 +0300

    auth intereptor

---
+++ app/src/main/java/com/lesson/foodamy/di/utlis/AuthInterceptor.kt
@@ -0,0 +1,29 @@
+package com.lesson.foodamy.di.utils
+
+import com.lesson.foodamy.preferences.IPrefDefaultManager
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * @author Mert Gölcü
+ * @date 8.03.2022
+ */
+
+
+
+class AuthInterceptor @Inject constructor(private val sharedPrefManager: IPrefDefaultManager) :
+    Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder = chain.request().newBuilder()
+        val token = sharedPrefManager.getToken()
+        if (token.isNotEmpty())
+            builder.addHeader(TOKEN_HEADER, token)
+        return chain.proceed(builder.build())
+    }
+
+    companion object {
+        const val TOKEN_HEADER = "X-Fodamy-Token"
+    }
+}
