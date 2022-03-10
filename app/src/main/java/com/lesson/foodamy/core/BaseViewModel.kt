797d890c7fde3f1b87616e01f6f04898f02a1adf
kemal <kmldeliceli@gmail.com>
2022-03-10 15:22:53 +0300

    sendRequest function created.

--- app/src/main/java/com/lesson/foodamy/core/BaseViewModel.kt
+++ app/src/main/java/com/lesson/foodamy/core/BaseViewModel.kt
@@ -1,12 +1,19 @@
 package com.lesson.foodamy.core
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavDirections
 import com.lesson.foodamy.SingleLiveEvent
+import com.lesson.foodamy.model.dataclass.BaseException
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.lang.Exception
 
 open class BaseViewModel : ViewModel() {
 
     val event = SingleLiveEvent<BaseViewEvent>()
+    val loading = MutableLiveData<Boolean>()
 
     fun sendEvent(ev: BaseViewEvent) {
         event.postValue(ev)
@@ -24,5 +31,44 @@ open class BaseViewModel : ViewModel() {
     }
     fun popBackStack() {
         sendEvent(BaseViewEvent.PopBackStack)
+    }
+    fun <T> sendRequest(
+        request: suspend ()->T,
+        success: ((T)->Unit),
+        error: ((Exception)->Unit)?=null,
+        loginDialog: (()->Unit)?=null,
+        loadingVal: Boolean = false
+    )= viewModelScope.launch {
+
+        loading.postValue(loadingVal)
+        try {
+            val response = request.invoke()
+            success.invoke(response)
+            loading.postValue(false)
+        }catch (e:Exception){
+            loading.postValue(false)
+            when(error){
+                null -> {
+                    handleException(e,loginDialog)
+                }
+            }
+        }
+
+
+    }
+
+    private fun handleException(e:Exception, loginDialog: (() -> Unit)?) {
+        when(e){
+            is IOException -> {
+                showMessage("No internet connection")
+            }
+            is BaseException ->{
+                if (e.code==403){
+                    loginDialog?.invoke()
+                }else{
+                    showMessage(e.error.toString())
+                }
+            }
+        }
     }
 }
